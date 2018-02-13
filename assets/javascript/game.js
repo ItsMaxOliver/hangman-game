c6cb266385d15611f95186e411fafe1f8d5e0ee3
MollyLJohnson <mollyl.johnson@att.net>
2018-02-13 17:27:24 -0600

    users now able to guess letters have them show up on screen and have them replace the underscores if they belong in the word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,38 @@
-Game = {};
-Game.userInput = [];
-Game.guessedLetters = [];
-Game.guesses = 6;
+var Game = { 
+blankSpaces : [], // used for underscores
+guessedLetters : [], // used to display the guessed letters
+guessesLeft : 6,
+moviesArr : [
+    "The Shawshank Redemption", "The Godfather", "Inception", "Pulp Fiction", "The Dark Knight", "Fight Club", "Goodfellas", "Casablanca", "Raiders of the Lost Ark", "The Silence of the Lambs", "Memento", "Forrest Gump", "Apocalypse", "Citizen Kane", "Taxi Driver"]
+};
 
-Game.moviesArr = [
-    "The Shawshank Redemption", "The Godfather", "Inception", "Pulp Fiction", "The Dark Knight", "Fight Club", "Goodfellas", "Casablanca", "Raiders of the Lost Ark", "The Silence of the Lambs", "Memento", "Forrest Gump", "Apocalypse", "Citizen Kane", "Taxi Driver"
-];
-
-Game.movie = Game.moviesArr[Math.floor(Math.random() * Game.moviesArr.length)]; // chooses a random word from moviesArr and assigns it to Game.movie
-console.log(Game.movie);// shows what word was picked in the console
-
-Game.guessCount = 6;
-
-
-for (var i = 0; i < Game.movie.length; i++) {
-    if (Game.movie[i] === " ") {
-        Game.userInput[i] = " "; // leaves a space between words in movie title
+window.onload = function showBlanks(){
+    Game.movie = Game.moviesArr[Math.floor(Math.random() * Game.moviesArr.length)]; // chooses a random word from moviesArr and assigns it to Game.movie
+    for (var i = 0; i < Game.movie.length; i++) {
+        if (Game.movie[i] === " ") {
+            Game.blankSpaces[i] = " "; // leaves a space between words in movie title
+        }
+        else {
+        Game.blankSpaces[i] = "_"; // changes letters to underscores
+        }
+    document.getElementById("blank-underscores").innerHTML = Game.blankSpaces.join(""); // displays the correct number of blanks
     }
-    else {
-    Game.userInput[i] = "_"; // changes letters to underscores
-    }
-    document.getElementById("blank-underscores").innerHTML = Game.userInput.join(""); // displays the correct number of blanks
 }
 
-document.onkeyup = function(event) {
-    for (var i = 0; i <= Game.guessCount; i++){   
-        var userGuess = event.key;
-        console.log(userGuess); // shows what letter was typed by the player in the console
-        Game.guessedLetters.push(userGuess); // pushes the userGuess into the guessedLetters array
-        console.log(Game.guessedLetters); // displays the guessedLetters array in console
-        document.getElementById("guessed-letters-list").innerHTML = Game.guessedLetters; // displays the guessed letters under the hangman box
-        
+document.onkeyup = function showGuessedLetters(event) {  
+    var userGuess = event.key;
+    Game.guessedLetters.push(userGuess); // pushes the userGuess into the guessedLetters array
+    document.getElementById("guessed-letters-list").innerHTML = Game.guessedLetters.join(", "); // displays the guessed letters under the hangman box
+}
+
+document.onkeydown = (function replaceBlank(event) {
+    var letter = event.key;    
         for (var i = 0; i < Game.movie.length; i++) {
-            if (userGuess == Game.movie[i]) {
+            if (letter === Game.movie[i].toLowerCase()) {
             //replace blank with userGuess
-            }
-            else {
-            //draw hangman body part  
+                Game.blankSpaces[i] = letter;
+               // replaceBlanks(Game.blankSpaces);
+                document.getElementById("blank-underscores").innerHTML = Game.blankSpaces.join("");
             }
         }
-        Game.guessCount = Game.guessCount - 1;
-        document.getElementById("guesses-left").innerHTML = Game.guessCount; // displays the number of guesses left
-}
-}
-
+});
