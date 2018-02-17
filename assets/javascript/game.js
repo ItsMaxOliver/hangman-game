dba87711ebeec881ea1cf258b21380125f2f881e
MollyLJohnson <mollyl.johnson@att.net>
2018-02-16 19:01:53 -0600

    have a check against repeated letter presses, win counter, win reset almost finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,12 @@ blankSpaces : [], // used for underscores
 guessedLetters : [], // used to display the guessed letters
 guessesLeft : 6,
 moviesArr : [
-    "The Shawshank Redemption", "The Godfather", "Inception", "Pulp Fiction", "The Dark Knight", "Fight Club", "Goodfellas", "Casablanca", "Raiders of the Lost Ark", "The Silence of the Lambs", "Memento", "Forrest Gump", "Apocalypse", "Citizen Kane", "Taxi Driver"],
+    "The Shawshank Redemption", "The Godfather", "Inception", "Pulp Fiction", "The Dark Knight", "Fight Club", "Goodfellas", "Casablanca", "Raiders of the Lost Ark", "The Silence of the Lambs", "Memento", "Forrest Gump", "Apocalypse Now", "Citizen Kane", "Taxi Driver"],
 hangmanArr : [
     "assets/images/hangman-arm2.png", "assets/images/hangman-arm1.png", "assets/images/hangman-leg2.png", "assets/images/hangman-leg1.png", "assets/images/hangman-body.png", "assets/images/hangman-head.png"
     ],
-numWins : 0
+numWins : 0,
+alreadyGuessed : false
 };
 
 function showBlanks() {
@@ -25,21 +26,19 @@ function showBlanks() {
 
 document.onkeyup = (function showGuessedLetters(event) {  
     var userGuess = event.key;
-        Game.guessedLetters.push(userGuess); // pushes the userGuess into the guessedLetters array
-        document.getElementById("guessed-letters-list").innerHTML = Game.guessedLetters.join(", "); // displays the guessed letters under the hangman box
-});
-
-//check that the user doesn't guess a letter more than once and that the counter doesn't count it against them if they do
-//
-
-document.onkeydown = (function replaceBlank(event) {
-    
-    var letter = event.key; 
     var found = false; // assumes that they will guess wrong
+    var alreadyGuessed = false;
+    
+    for (var j = 0; j < Game.guessedLetters.length; j++) {
+        if (userGuess === Game.guessedLetters[j]) {
+            alreadyGuessed = true;
+        }
+    }
+    if(alreadyGuessed === false) {
         for (var i = 0; i < Game.movie.length; i++) {
-            if (letter === Game.movie[i].toLowerCase()) {
+            if (userGuess === Game.movie[i].toLowerCase()) {
                //replace blank with letter
-                Game.blankSpaces[i] = letter;
+                Game.blankSpaces[i] = userGuess;
                 document.getElementById("blank-underscores").innerHTML = Game.blankSpaces.join("");
                 found = true; // they got the letter correct so no deduction
             }
@@ -48,8 +47,15 @@ document.onkeydown = (function replaceBlank(event) {
             Game.guessesLeft = Game.guessesLeft - 1;
             document.getElementById("guesses-left").innerHTML = Game.guessesLeft; // displays new number of guesses left
             document.getElementById("hangman").src = Game.hangmanArr[Game.guessesLeft]; // replaces the image of the hangman when the letter they guess doesn't match one in the random word
-        }  
+        }
+        Game.guessedLetters.push(userGuess); // pushes the userGuess into the guessedLetters array
+        document.getElementById("guessed-letters-list").innerHTML = Game.guessedLetters.join(", "); // displays the guessed letters under the hangman box
+    }
+    
 });
+
+//check that the user doesn't guess a letter more than once and that the counter doesn't count it against them if they do
+//
 
 //end game when guessesLeft = 0
 var delay = setInterval(deadAlert, 1500); // delays the function so that the entire hangman is seen
@@ -67,16 +73,19 @@ var win = setInterval(winAlert, 1500); // delays the function so that the user s
 
 function winAlert() {
     if (Game.movie.toLowerCase() === Game.blankSpaces.join("")) {
-        alert("You WIN!"); //alerts user that they won
-        clearInterval(win); // makes it so that the alert doesn't repeat
+        alert("You WIN! " + Game.movie + " was the correct answer! Good job!"); //alerts user that they won
+        //clearInterval(win);
         Game.numWins = Game.numWins + 1;
         document.getElementById("number-of-wins").innerHTML = Game.numWins;
-        showBlanks(); // resets the blanks
-        //Game.guessedLetters = [];
-        //Game.guessesLeft = 6;
-        //window.location.reload(); // makes the window refresh to play a new game
-        
+        Game.guessedLetters = [];
+        document.getElementById("blank-underscores").innerHTML = " "; // this is where we need to set it equal to a string that is as long as the longest item in the moviesArr
+        document.getElementById("guessed-letters-list").innerHTML = Game.guessedLetters;
+        Game.guessesLeft = 6;
+        document.getElementById("guesses-left").innerHTML = Game.guessesLeft; // displays new number of guesses left
+        document.getElementById("hangman").src = "assets/images/hangman-base.png"; // replaces the image of the hangman when the letter they guess doesn't match one in the random word
+        showBlanks(); // resets the blanks 
     }
+    win;
 };
 
 window.onload = showBlanks();
